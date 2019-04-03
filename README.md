7e0aab0f35f9b43607a1da4c11d9e9546805500d
sergio-20 <nubesgladius20@gmail.com>
2019-04-03 15:37:01 -0700

    Updated README.md file

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # This is a website I made using React.js
 
+##Screenshots
+
+> ![react-website-discover](https://user-images.githubusercontent.com/29030325/55517701-2457f180-5626-11e9-9288-782cee01f856.png)
+
+> ![react-website-discover2](https://user-images.githubusercontent.com/29030325/55517706-25891e80-5626-11e9-825b-07ce5899eba1.png)
+
 ## View Online:
  > https://sergio-20.github.io/React-Website/
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import { Router } from 'react-router';
-import createHashHistory from 'history/createHashHistory';
-
 import Main from './Main';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -14,14 +11,5 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render( <Main />, document.getElementById( 'main' ) );
 ReactDOM.render( <Navbar />, document.getElementById( 'navbar' ) );
 ReactDOM.render( <Footer />, document.getElementById( 'footer' ) );
-
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
-
- ReactDOM.render(
-     <Router history={hashHistory}>
-       <App />
-     </Router>,
-   document.getElementById('root') as HTMLElement
- );
 
 serviceWorker.unregister();
